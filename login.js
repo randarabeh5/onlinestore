a75b61a960b41c35cad1c7cc947211ce1e76a433
randarabeh5 <2301093@rst.edu.eg>
2025-08-07 00:32:39 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const error = document.getElementById("errorMsg");
+
+  if (username === "admin" && password === "1234") {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "index.html";
+  } else {
+    error.textContent = "Invalid username or password.";
+  }
+});
 
